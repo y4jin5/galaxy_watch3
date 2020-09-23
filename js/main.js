97fabfc0ac8b90b7d09b75027e2e97cbc067fdfd
Nyuuuu222 <lolollll1025@gmail.com>
2020-09-23 19:06:40 +0900

    20200923
    20200923

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 new WOW().init();
+
+// if (jQuery(window).width() > 0) {
+//     jQuery(window).on("scroll",function(ev){
+//         if(jQuery(window).scrollTop() > 150 ) { 
+//             jQuery('.menu_box').addClass('navbar-inverse');
+
+//         }
+//         else{
+//             jQuery('.menu_box').removeClass('navbar-inverse');
+
+            
+//         }
+//         return false;
+//     });
+//     }
